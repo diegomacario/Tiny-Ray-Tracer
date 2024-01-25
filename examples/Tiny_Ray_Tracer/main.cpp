27a2fbc3bac060f3605b427d1123abe27590eba3
Diego Macario <diego.macario5@gmail.com>
2024-01-24 20:16:27 -0500

    Add scene description of planet.

--- examples/Tiny_Ray_Tracer/main.cpp
+++ examples/Tiny_Ray_Tracer/main.cpp
@@ -22,7 +22,7 @@ LilyGo_Class amoled;
 #define WIDTH  amoled.height()
 #define HEIGHT amoled.width()
 
-FileParser fileParser(castleSceneDescription);
+FileParser fileParser(planetSceneDescription);
 std::unique_ptr<SceneDescription> sceneDesc = nullptr;
 std::unique_ptr<Scene> scene = nullptr;
 RandomSampleGenerator sampleGenerator;
