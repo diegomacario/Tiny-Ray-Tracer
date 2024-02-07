da9176b13780d1f2d9b5512de18222ebe4dccb2c
Diego Macario <diego.macario5@gmail.com>
2024-02-07 08:51:41 -0500

    Add skeleton of MenuState.

---
+++ examples/Tiny_Ray_Tracer/MenuState.h
@@ -0,0 +1,97 @@
+#ifndef MENU_STATE_H
+#define MENU_STATE_H
+
+#include <LilyGo_AMOLED.h>
+#include <TFT_eSPI.h>
+
+#include "State.h"
+#include "FSM.h"
+#include "FileParser.h"
+#include "RandomSampleGenerator.h"
+#include "RayGenerator.h"
+#include "SceneDescription.h"
+
+struct TextSpriteSettings {
+    TextSpriteSettings(int32_t textSize, int32_t fontHeight, int32_t fontWidth, std::string longestPossibleString)
+        : textSize(textSize)
+        , fontHeight(fontHeight)
+        , fontWidth(fontWidth)
+        , numCharacters(longestPossibleString.length())
+        , spriteWidth(fontWidth * textSize * numCharacters)
+        , spriteHeight(fontHeight * textSize)
+    { }
+
+    int32_t textSize;
+    int32_t fontHeight;
+    int32_t fontWidth;
+    int32_t numCharacters;
+    int32_t spriteWidth;
+    int32_t spriteHeight;
+};
+
+class MenuState : public State
+{
+public:
+
+   MenuState(const std::shared_ptr<FiniteStateMachine>& finiteStateMachine,
+             LilyGo_Class* amoled,
+             TFT_eSPI& tft,
+             uint16_t screenWidth,
+             uint16_t screenHeight);
+   ~MenuState() = default;
+
+   MenuState(const MenuState&) = delete;
+   MenuState& operator=(const MenuState&) = delete;
+
+   MenuState(MenuState&&) = delete;
+   MenuState& operator=(MenuState&&) = delete;
+
+   void enter() override;
+   void update() override;
+   void exit() override;
+
+private:
+
+   void updateRayTracingSprite();
+   void updatePercentageProgressSprite(float progress);
+   void updateProgressBar(float progress);
+
+   std::shared_ptr<FiniteStateMachine> mFSM;
+
+   LilyGo_Class*                       amoled;
+
+   const uint16_t                      mScreenWidth;
+   const uint16_t                      mScreenHeight;
+
+   TFT_eSprite                         mImageRenderingSprite;
+   TFT_eSprite                         mRayTracingLabelSprite;
+   TFT_eSprite                         mPercentageProgressLabelSprite;
+   TFT_eSprite                         mProgressBarSprite;
+
+   FileParser                          mFileParser;
+   std::unique_ptr<SceneDescription>   mSceneDesc;
+   std::unique_ptr<Scene>              mScene;
+   RandomSampleGenerator               mSampleGenerator;
+   RayGenerator                        mRayGenerator;
+   Sample                              mSample;
+   Ray                                 mRay;
+   Intersection                        mIntersection;
+
+   TextSpriteSettings                  mRayTracingSpriteSettings;
+   uint32_t                            mNumDots;
+   unsigned long                       mLastTimeRayTracingSpriteWasUpdated;
+
+   TextSpriteSettings                  mPercentageProgressSpriteSettings;
+
+   const int32_t                       mProgressBarWidth;
+   const int32_t                       mProgressBarHeight;
+   const int32_t                       mProgressBarXPosition;
+   const int32_t                       mProgressBarYPosition;
+   const int32_t                       mProgressBarFillableWidth;
+   const int32_t                       mProgressBarFillableHeight;
+   int32_t                             mPrevProgressWidth;
+
+   bool                                mDoOnce;
+};
+
+#endif
