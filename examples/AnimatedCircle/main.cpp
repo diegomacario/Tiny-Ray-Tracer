236105de6a42a2fee2c48617331368f0d70a305a
Diego Macario <diego.macario5@gmail.com>
2024-03-02 11:32:22 -0500

    Add landscape code that allows me to reproduce the tearing bug.

--- examples/AnimatedCircle/main.cpp
+++ examples/AnimatedCircle/main.cpp
@@ -4,34 +4,29 @@
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite img = TFT_eSprite(&tft);
 
+unsigned int colorIndex = 0;
+unsigned long frameCounter = 0;
+
 void setup() {
   img.createSprite(536, 240);
-  img.setSwapBytes(1);
   rm67162_init();
   lcd_setRotation(1);
 }
 
-int x = 0;
-
-void loop() {  
-  //img.fillCircle(x, 120, 90, TFT_BLACK);
-  img.drawCircle(x, 120, 90, TFT_BLACK);
-  x = x + 1;
-  if (x > 536) x = 0;
-
-  if (x >= 200 && x < 400) {
-    //img.fillCircle(x, 120, 90, TFT_GREEN);
-    img.drawCircle(x, 120, 90, TFT_GREEN);
-  } else if (x >= 400 && x < 536) {
-    //img.fillCircle(x, 120, 90, TFT_BLUE);
-    img.drawCircle(x, 120, 90, TFT_BLUE);
-  } else {
-    //img.fillCircle(x, 120, 90, TFT_RED);
-    img.drawCircle(x, 120, 90, TFT_RED);
+void loop() {
+  if (colorIndex == 0) {
+    img.fillRect(0, 0, 536, 240, TFT_RED);
+  } else if (colorIndex == 1) {
+    img.fillRect(0, 0, 536, 240, TFT_GREEN);
+  } else if (colorIndex == 2) {
+    img.fillRect(0, 0, 536, 240, TFT_BLUE);
   }
 
-  //img.fillRect(18, 70, 100, 100, TFT_BLACK);
-  //img.drawString(String(x), 20, 74, 7);
-
   lcd_PushColors(0, 0, 536, 240, (uint16_t*)(img.getPointer()));
+
+  frameCounter += 1;
+  if (frameCounter % 180 == 0) {
+    colorIndex += 1;
+    colorIndex %= 3;
+  }
 }
